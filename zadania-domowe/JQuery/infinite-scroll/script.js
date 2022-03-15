056242f3919f98a33495296ea72a557ab7c7011e
Roman Zbroiński <romanzbroinski@gmail.com>
2022-03-15 21:59:48 +0100

    warsztat ajax 4 - infinite scroll

---
+++ zadania-domowe/JQuery/infinite-scroll/script.js
@@ -0,0 +1,70 @@
+let endOfThePage = 0;
+let preloading = false;
+
+
+const showPreloader = () => {
+    let preloader = document.getElementById('preloader')
+    preloader.style.display ='block';
+}
+
+const hidePreloader = () => {
+    let preloader = document.getElementById('preloader')
+    preloader.style.display ='none';
+}
+
+
+
+const getData = () => {
+    if(!preloading){
+        //bez funkcji preloadingu
+    }
+    preloading = true;
+   fetch('https://akademia108.pl/api/ajax/get-users.php')
+   .then(res=>res.json())
+   .then(data => {
+    let body = document.body;
+    let hr = document.createElement('hr');
+    body.appendChild(hr);
+       for(let user of data) {
+           let pId = document.createElement('p');
+           let pName = document.createElement('p');
+           let pWebsite = document.createElement('p');
+
+           pId.innerText = `User ID: ${user.id}`;
+           pName.innerText = `Name: ${user.name}`;
+           pWebsite.innerHTML = `User Website: ${user.pWebsite}<br />----------`;
+
+           let body = document.body;
+           body.appendChild(pId);
+           body.appendChild(pName);
+           body.appendChild(pWebsite);
+
+       }
+       hidePreloader();
+        console.log(data);
+   })
+   .catch(error => {
+       console.log(error);
+   })
+}
+const   scrollToEndPage = () => {
+    let d = document.documentElement;
+    //height of an element's content, incuding content
+    //not visible on the screen
+    let scrollHeight = d.scrollHeight;
+    //number of pixels that an element's content 
+    //sxcrolled vertically
+    let scrollTop = d.scrollTop;
+    //inner height of an element in pixels
+    let clientHeight = d.clientHeight;
+
+    let sumScrollTopClientHeight = Math.ceil(scrollTop + clientHeight);
+
+    if(sumScrollTopClientHeight>=scrollHeight){
+        endOfThePage += 1;
+        console.log(`Finish page ${endOfThePage}`);
+    }
+    showPreloader();
+    getData();
+}
+window.addEventListener('scroll', scrollToEndPage);
--- zadania-domowe/JQuery/wyplatySuma/main.js
+++ zadania-domowe/JQuery/wyplatySuma/main.js
@@ -1,6 +1,7 @@
+/*
 var sumKa = function(){
     $(document).ready(function(){
-    var wyplata = 0;
+    let wyplata = 0;
     $('.salary').each(function(){
         wyplata += parseInt($(this).text());
         return wyplata;
@@ -8,3 +9,15 @@ var sumKa = function(){
    console.log(wyplata);
 });
 }
+*/
+$('#count-sum').click(function() {
+    $(document).ready(function(){
+        let wyplata = 0;
+        $('.salary').each(function(){
+            wyplata += parseInt($(this).text());
+            return wyplata;
+       });
+       $('#sum').html(wyplata)
+       //console.log(wyplata);
+    });
+});
